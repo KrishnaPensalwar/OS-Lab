a7f675d2de8deaceafbe3b53b589a725cf522ac7
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2023-11-21 12:05:43 +0530

    RoundRobin.c

--- RoundRobin.c
+++ RoundRobin.c
@@ -1,80 +1,81 @@
 Name :Krishna Rajendra Pensalwar
 Roll No. : TIA54
 
+#include "iostream"
+using namespace std;
 
-#include<stdio.h>
-#include<stdlib.h>
+void rr(int num,int bt[],int tq);
 
-int main() {
-    int num, quant_time;
-    float avg_wt = 0, avg_tat = 0;
-    int sq = 0;
-    int bt[10], rem_bt[10], wt[10], tat[10];
+int main(){
+    int num,tq;
 
-    printf("Enter number of processes = ");
-    scanf("%d", &num);
+    cout<<"Enter number of processes";
+    cin>>num;
 
-    printf("\nEnter burst time of all processes = ");
+    int bt[num];
+    cout<<"Enter processes";
     for (int i = 0; i < num; i++) {
-        scanf("%d", &bt[i]);
-        rem_bt[i] = bt[i];
+        cout<<"ENTER BT OF "<<i+1;
+        cin>>bt[i];
     }
-    printf("Enter the Time-Quantum = ");
-    scanf("%d", &quant_time);
 
-    while (1) {
-        int flag = 0;
-        for (int i = 0; i < num; i++) {
-            if (rem_bt[i] > 0) {
-                flag = 1;
-                if (rem_bt[i] > quant_time) {
-                    rem_bt[i] -= quant_time;
-                    sq += quant_time;
-                } else {
-                    sq += rem_bt[i];
-                    tat[i] = sq;
-                    rem_bt[i] = 0;
-                }
-            }
-        }
+    cout<<"Enter time qu";
+    cin>>tq;
 
-        if (flag == 0) {
-            break;
-        }
-
-        printf("\nProcess\tBurst Time\tTurnaround Time\tWaiting time\n");
-        for (int i = 0; i < num; i++) {
-            wt[i] = tat[i] - bt[i];
-            avg_tat += tat[i];
-            avg_wt += wt[i];
-            printf("%d\t%d\t%d\t%d\n", i + 1, bt[i], tat[i], wt[i]);
-        }
-
-        avg_wt /= num;
-        avg_tat /= num;
-        printf("Average Waiting-Time = %f\n", avg_wt);
-        printf("Average Turnaround-Time = %f\n", avg_tat);
-    }
+    rr(num,bt,tq);
 
     return 0;
+
 }
 
+void rr(int num,int bt[],int tq){
+    int rem_bt[num],wt[num],tat[num];
 
---------------------------------------------------------------------OUTPUT------------------------------------------------------
+    float avgwt=0;
+    float avgtat=0;
 
+    int te=0;
 
-/*
-Enter number of processes = 2
+    for (int i = 0; i < num; i++) {
+        rem_bt[i]=bt[i];
+    }
+    while(1){
+        int flag=0;
 
-Enter burst time of all processes = 4 3 5
-Enter the Time-Quantum = 2
+        for (int i = 0; i < num; i++) {
+            if(rem_bt[i]>0){
+                flag=1;
 
-Process Burst Time    Turnaround-Time Waiting time
-1           4                  9          5
-2           3                  7          4
-3           5                  12         7
+                if(rem_bt[i] > tq){
+                    rem_bt[i] -= tq;
+                    te +=tq;
 
-Average Waiting-Time = 5.33
-Average Turnaround-Time = 9.33
+                }else{
+                    te+= rem_bt[i];
+                    tat[i]=te;
+                    rem_bt[i]=0;
 
-*/
+                }
+            }
+
+        }
+       if(flag==0){
+           break;
+       }
+
+    }
+    cout<<"Process\tBT\tWT\tTAT\n";
+    for (int i = 0; i < num; i++) {
+        wt[i] = tat[i] - bt[i];
+        avgwt +=wt[i];
+        avgtat+=tat[i];
+        cout<<i+1<<"\t\t"<<bt[i]<<"\t"<<wt[i]<<"\t"<<tat[i]<<"\n";
+    }
+
+    avgwt /= num;
+    avgtat /= num;
+
+    printf("Average Waiting-Time = %f\n", avgwt);
+    printf("Average Turnaround-Time = %f\n", avgtat);
+
+}
